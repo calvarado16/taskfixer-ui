d4501059b4071be1cd8a0c23d342b3a7cccac92b
caalv <caalvaradof04@gmail.com>
2025-08-14 22:32:08 -0600

    Fixing  horas

--- src/components/ServiceOfferingsList.jsx
+++ src/components/ServiceOfferingsList.jsx
@@ -87,7 +87,7 @@ const ServiceOfferingsList = () => {
                 Precio
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
-                Duración (min)
+                Duración (hrs)
               </th>
               <th className="px-6 py-3 text-left text-xs font-medium text-gray-400 uppercase tracking-wider">
                 Estado
